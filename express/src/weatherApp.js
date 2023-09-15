58fed18c680d6d14e0ab109a87f3feee9f0cf89b
AGS Lakpahana <agslakpahana@gmail.com>
2023-09-15 21:08:57 +0530

    work using uni proxy

--- express/src/weatherApp.js
+++ express/src/weatherApp.js
@@ -1,7 +1,20 @@
 const request = require('request');
+const fetch = require('node-fetch');
+const HttpsProxyAgent = require('https-proxy-agent');
 
 const weatherApp = (address) => {
     return new Promise((resolve, reject) => {
+
+        //TODO
+
+        // use proxy
+
+        // const HTTP_PROXY = 'http://10.50.225.222:3128';
+
+
+
+
+
 
         const API_KEY = 'b71cb84f8ee94e599f985343232408';
         const info = {
@@ -20,6 +33,23 @@ const weatherApp = (address) => {
             }
             resolve(response.body);
         })
+
+        // fetch(info.url, {
+        //     method: 'GET',
+        //     headers: {
+        //         'Content-Type': 'application/json',
+        //         'Accept': 'application/json',
+        //         'User-Agent': 'Mozilla/5.0',
+        //         'Access-Control-Allow-Origin': '*'
+        //     },
+        //     agent: new HttpsProxyAgent(HTTP_PROXY)
+        // })
+
+        //     .then(res => res.json())
+        //     .then(json => {
+        //         resolve(json);
+        //     }
+        //     )
     }
     )
 }
